1cd85bb010343545a3c5e294ee042d2df3d4d353
MajorMajormajor124 <anguyen68@wisc.edu>
2025-10-27 23:48:42 -0500

    Introduction Screen

--- app/src/main/java/com/cs407/cubemaster/MainActivity.kt
+++ app/src/main/java/com/cs407/cubemaster/MainActivity.kt
@@ -3,27 +3,47 @@ package com.cs407.cubemaster
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cs407.cubemaster.ui.theme.Blue
 import com.cs407.cubemaster.ui.theme.CubemasterTheme
+import com.cs407.cubemaster.ui.theme.DarkYellow
+import com.cs407.cubemaster.ui.theme.Orange
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             CubemasterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Orange
+                ) {
+                    StartScreen()
                 }
             }
         }
@@ -31,17 +51,69 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun StartScreen(modifier: Modifier = Modifier) {
+    Box(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .background(Orange),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .size(200.dp)
+                .shadow(elevation = 16.dp, shape = CircleShape)
+                .clip(CircleShape)
+                .background(DarkYellow)
+        )
+        Column(
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(100.dp))
+            Box {
+                Text(
+                    text = "CUBEMASTER",
+                    style = TextStyle(
+                        fontSize = 48.sp,
+                        color = Blue
+                    ),
+                    modifier = Modifier.padding(start = 4.dp, top = 4.dp)
+                )
+                Text(
+                    text = "CUBEMASTER",
+                    style = TextStyle(
+                        fontSize = 48.sp,
+                        color = Color.White
+                    )
+                )
+            }
+            Spacer(modifier = Modifier.height(32.dp))
+            Text(
+                text = "Happy Solving!",
+                style = TextStyle(
+                    fontSize = 32.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                onClick = { /* TODO */ },
+                modifier = Modifier
+                    .padding(bottom = 64.dp)
+                    .size(width = 200.dp, height = 80.dp)
+            ) {
+                Text(text = "Start", fontSize = 24.sp)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun StartScreenPreview() {
     CubemasterTheme {
-        Greeting("Android")
+        StartScreen()
     }
 }
